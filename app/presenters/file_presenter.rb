eb928490019fe14607c3e50111c0c556c348af9a
luisgreg99 <luisram90_101@hotmail.com>
2015-07-24 11:30:55 -0700

    updated file presenter to display labels instead of uris

--- app/presenters/file_presenter.rb
+++ app/presenters/file_presenter.rb
@@ -1,4 +1,18 @@
 # app/presenters/file_presenter.rb
 class FilePresenter < Sufia::GenericFilePresenter
   self.terms += ScholarsArchiveSchema.properties.map(&:name)
+  def [](key)
+    result = Array.wrap(super)
+    result.flat_map{|x| clean_item(x) }
+  end
+
+  private
+
+  def clean_item(item)
+    if item.kind_of?(Hash) && item["preflabel"]
+      item["preflabel"]
+    else
+      item
+    end
+  end
 end
