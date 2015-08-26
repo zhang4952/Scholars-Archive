8a23063a5c387fd58309788ccceebebe5cabb93c
luisgreg99 <luisram90_101@hotmail.com>
2015-08-26 15:26:58 -0700

    added view with date type list for ingest form

--- app/presenters/file_presenter.rb
+++ app/presenters/file_presenter.rb
@@ -1,6 +1,7 @@
 # app/presenters/file_presenter.rb
 class FilePresenter < Sufia::GenericFilePresenter
   self.terms += ScholarsArchiveSchema.properties.map(&:name) + [:nested_authors]
-  self.terms -= [:subject, :keyword]
+  self.terms -= [:subject, :keyword, :accepted, :available, :copyrighted,:created, :issued,:submitted,:modified, :valid_date]
+
   delegate :nested_authors_attributes=, :to => :model
 end
--- app/schemas/scholars_archive_schema.rb
+++ app/schemas/scholars_archive_schema.rb
@@ -7,5 +7,14 @@ class ScholarsArchiveSchema < ActiveTriples::Schema
   property :doi, predicate: ::RDF::Vocab::Identifiers.doi
   property :orcid, predicate: ::RDF::Vocab::Identifiers.orcid
   property :rid, predicate: ::RDF::Vocab::Identifiers.rid
+  property :accepted, predicate: ::RDF::DC.dateAccepted
+  property :available, predicate: ::RDF::DC.available
+  property :copyrighted, predicate: ::RDF::DC.dateCopyrighted
+  # property :collected, predicate: ::RDF::DC.
+  property :created, predicate: ::RDF::DC.created
+  property :issued, predicate: ::RDF::DC.issued
+  property :submitted, predicate: ::RDF::DC.dateSubmitted
+  property :modified, predicate: ::RDF::DC.modified #updated
+  property :valid_date, predicate: ::RDF::DC.valid
 
 end
