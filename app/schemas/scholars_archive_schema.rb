3282660824a5be8013c5f24a2ed6859461860f34
luisgreg99 <luisram90_101@hotmail.com>
2015-08-26 15:46:08 -0700

    updated schema

--- app/schemas/scholars_archive_schema.rb
+++ app/schemas/scholars_archive_schema.rb
@@ -11,10 +11,7 @@ class ScholarsArchiveSchema < ActiveTriples::Schema
   property :available, predicate: ::RDF::DC.available
   property :copyrighted, predicate: ::RDF::DC.dateCopyrighted
   # property :collected, predicate: ::RDF::DC.
-  property :created, predicate: ::RDF::DC.created
   property :issued, predicate: ::RDF::DC.issued
-  property :submitted, predicate: ::RDF::DC.dateSubmitted
-  property :modified, predicate: ::RDF::DC.modified #updated
   property :valid_date, predicate: ::RDF::DC.valid
 
 end
